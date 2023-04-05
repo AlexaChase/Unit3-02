b3dfc20cf917350c0c2f2af95b649955cf282331
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-04-05 16:54:36 +0000

    new changes

--- program.py
+++ program.py
@@ -1,13 +1,13 @@
 lengthA = float(input("Please enter length A of the trapezoid:"))
-lengthB = float(input("Please enter length B of the trapezoid:"))
-lengthH = float(input("Please enter the height of the trapezoid:"))
-AreaTrapezoid= lengthH * (lengthA + lengthB) /2
 if lengthA <= 0:
     print("Error, number must be positive")
-    if lengthB <= 0:    
-        print("Error, number must be positive")
-        if lengthH <= 0:
-            print("Error, number must be positive")
-elif AreaTrapezoid> 0:
+lengthB = float(input("Please enter length B of the trapezoid:"))
+if lengthB <= 0:
+    print("Error, number must be positive")
+lengthH = float(input("Please enter the height of the trapezoid:"))
+if lengthH <= 0:
+    print("Error, number must be positive")
+AreaTrapezoid= lengthH * (lengthA + lengthB) /2
+if AreaTrapezoid> 0:
     print("The area of the trapezoid:")
     print(AreaTrapezoid)
