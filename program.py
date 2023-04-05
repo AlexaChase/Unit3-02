ec4f5b8e6e53aaec7c0506d19b26b41c5498878f
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-04-05 16:42:10 +0000

    new changes

--- program.py
+++ program.py
@@ -1,1 +1,6 @@
-Hello, World!
+lengthA = float(input("Please enter length A of the trapezoid:"))
+lengthB = float(input("Please enter length B of the trapezoid:"))
+lengthH = float(input("Please enter the height of the trapezoid:"))
+AreaTrapezoid= lengthH * (lengthA + lengthB) /2
+print("The area of the trapezoid:")
+print(AreaTrapezoid)
